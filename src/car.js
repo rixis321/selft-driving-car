cdecebf3b30016bbebb003f4af17414cd0dd942f
Konrad Sygut <konrsyg825@student.polsl.pl>
2022-10-06 22:46:45 +0200

    sensor implemented, collision todo

--- src/car.js
+++ src/car.js
@@ -28,9 +28,9 @@ class Car {
     this.sensor.draw(ctx);
   }
 
-  update() {
+  update(roadBorders) {
     this.#move();
-    this.sensor.update();
+    this.sensor.update(roadBorders);
   }
 
   #move() {
