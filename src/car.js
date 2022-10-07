6a28a2f37dde3e8347b0398c179ce1c22461e1a6
Konrad Sygut <konrsyg825@student.polsl.pl>
2022-10-07 23:04:02 +0200

    added buttons and visualized network

--- src/car.js
+++ src/car.js
@@ -10,17 +10,18 @@ class Car {
     this.maxSpeed = maxSpeed;
     this.friction = 0.05; //tarcie
     this.angle = 0;
+    this.damaged = false;
+    this.useBrain = controlType === "AI";
 
     if (controlType !== "DUMMY") {
       this.sensor = new Sensor(this);
+      this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
     }
 
     this.controls = new Controls(controlType);
-
-    this.damaged = false;
   }
 
-  draw(ctx, color) {
+  draw(ctx, color, drawSensor = false) {
     if (this.damaged) {
       ctx.fillStyle = "gray";
     } else {
@@ -32,7 +33,7 @@ class Car {
       ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
     }
     ctx.fill();
-    if (this.sensor) {
+    if (this.sensor && drawSensor) {
       this.sensor.draw(ctx);
     }
   }
@@ -45,6 +46,17 @@ class Car {
     }
     if (this.sensor) {
       this.sensor.update(roadBorders, traffic);
+      const offsets = this.sensor.readings.map((s) =>
+        s === null ? 0 : 1 - s.offset
+      );
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+      if (this.useBrain) {
+        this.controls.up = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.back = outputs[3];
+      }
     }
   }
 
